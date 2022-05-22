eaa9567bc9ce6312693e3aa4af1b64f7fef43c08
chungen0126 <65023303+chungen0126@users.noreply.github.com>
2022-05-22 23:14:04 +0800

    Suffix to use from right table overlapping columns. (#15)
    * Suffix to use from right table overlapping columns.
    
    * Suffix to use from right table overlapping columns.
    
    * Suffix to use from right table overlapping columns.
    
    * Suffix to use from right table overlapping columns.

--- web/flask/static/js/new_task_page.js
+++ web/flask/static/js/new_task_page.js
@@ -306,11 +306,22 @@ $(document).ready(function() {
             key        = l.eq(i).attr('id').substring('typeopt_'.length);
             opt        = l.eq(i).val();
             is_primary = $(`input[id=isprimary_${key.replace(/[:.]/g, "_").replace(/[@]/g, "")}]`).prop('checked')
-            key_info[to_formatted_key(key)] = {
-                'key': to_formatted_key(key),
+
+            new_key = to_formatted_key(key);
+            if(new_key in key_info){
+                var inc = 1;
+                new_key = to_formatted_key(key) + inc.toString()
+                while(new_key in key_info){
+                    inc += 1;
+                    new_key = to_formatted_key(key) + inc.toString()
+                }
+            }
+            key_info[new_key] = {
+                'key': new_key,
                 'type': opt,
                 'is_primary': is_primary
             }
+
         }
 
         try {
@@ -597,7 +608,7 @@ function sql_gen_join(join_pairs) {
     /* df1 is the table of db2 */
 
     sql = 'SELECT ';
-
+    var key_array = [];
     for (i = 0; i < join_pairs.length; ++i) {
         idx      = join_pairs[i]['idx'];
         leftkey  = to_formatted_key(join_pairs[i]['leftkey']);
@@ -607,14 +618,31 @@ function sql_gen_join(join_pairs) {
             alert("Left key & Right key cannot be both empty");
             throw "Left key & Right key cannot be both empty";
         }
-        else if (leftkey != '' && rightkey == '') {
-            sql += 'df0.' + leftkey + ' as ' + leftkey + ', '
-        }
-        else if (leftkey == '' && rightkey != '') {
-            sql += 'df1.' + rightkey + ' as ' + rightkey + ', '
-        }
         else {
-            sql += 'COALESCE(df0.' + leftkey + ', df1.' + leftkey + ') as ' + leftkey + ', ';
+            var key;
+            if (leftkey != '' && rightkey == '') {
+                key = leftkey;
+                sql += 'df0.' + leftkey  + ' as ';
+            }
+            else if(leftkey == '' && rightkey != ''){
+                key = rightkey;
+                sql += 'df1.' + rightkey + ' as ';
+            }
+            else{
+                key = leftkey;
+                sql += 'COALESCE(df0.' + leftkey + ', df1.' + rightkey + ') as ' ;
+            }
+
+            var new_key = '';
+            new_key = to_formatted_key(key);
+            if (key_array.includes(new_key)) {
+                var inc = 1;
+                while (key_array.includes(new_key = to_formatted_key(key) + inc.toString())){
+                    inc += 1;
+                }
+            }
+            sql += new_key + ', ';
+            key_array.push(new_key);
         }
     }
 
