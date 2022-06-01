ab8d4ec10cb0b1d87d5e4866d13737866235395d
chungen0126 <65023303+chungen0126@users.noreply.github.com>
2022-06-01 17:19:21 +0800

    Fix task status (#18)
    * Fix task status
    
    * Update demo.py
    
    Co-authored-by: Chiao-Wei Wang <76680670+CW-B-W@users.noreply.github.com>

--- dcfs/dcfs-share/dcfs-run/demo.py
+++ dcfs/dcfs-share/dcfs-run/demo.py
@@ -425,8 +425,9 @@ try:
             'from': 'local:///',
             'to':   'hds:///csv/join/'+ table_name.upper() +  '.csv'
     }
-    r = requests.post(url, params=params, data=open(tmp_csv_path, 'rb') , timeout=10)
-    send_task_status(task_id, TASKSTATUS_SUCCEEDED, "Finished importing csv file into HDS", '/dataservice/v1/access?from=hds:///csv/join/' + table_name.upper() + '.csv&to=local:///result.csv&redirectfrom=NULL')
+    with open(tmp_csv_path, 'rb') as fp:
+        requests.post(url, params=params, data=fp , timeout=10)
+    send_task_status(task_id, TASKSTATUS_PROCESSING, "Finished importing csv file into HDS", '/dataservice/v1/access?from=hds:///csv/join/' + table_name.upper() + '.csv&to=local:///result.csv&redirectfrom=NULL')
     logging.info("Finished importing csv file into HDS")
 except Exception as e:
     logging.error("Error importing csv file into HDS. Please check HDS regionserver: " + str(e))
@@ -463,15 +464,15 @@ if task_dict['phoenix']=='true':
         logging.error("Failed to import table into HDS\n" + stderr)
         send_task_status(task_id, TASKSTATUS_FAILED, "Failed to import table into HDS\n" + stderr, '')
         exit(1)
+    elif stderr.find("ERROR") != -1:
+        logging.error("Job finished with error message: \n" + stderr)
+        send_task_status(task_id, TASKSTATUS_FAILED, "Job finished with error message: \n" + stderr, '')
+        exit(1)
     else:
         logging.error("Successfully importing table into HDS" + stderr)
         send_task_status(task_id, TASKSTATUS_PROCESSING, "Successfully importing table into HDS", '')
-    ''' ========== Phoenix ========== '''
+''' ========== Phoenix ========== '''
 
-    if stderr.find("ERROR") == -1:
-        logging.error("Job finished")
-        send_task_status(task_id, TASKSTATUS_SUCCEEDED, "Job finished.", '/dataservice/v1/access?from=hds:///csv/join/' + table_name.upper() + '.csv&to=local:///result.csv&redirectfrom=NULL')
-    else:
-        logging.error("Job finished with error message: \n" + stderr)
-        send_task_status(task_id, TASKSTATUS_SUCCEEDED, "Job finished with error message: \n" + stderr, '')
+logging.error("Job finished")
+send_task_status(task_id, TASKSTATUS_SUCCEEDED, "Job finished.", '/dataservice/v1/access?from=hds:///csv/join/' + table_name.upper() + '.csv&to=local:///result.csv&redirectfrom=NULL')
 exit()
